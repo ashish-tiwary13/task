3390983a42bd36be4b9517a4672316a65e520456
ashish-tiwary13 <ashishtiwary1326@gmail.com>
2023-04-12 01:38:07 +0530

    removed unwanted  files

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import {Animated,FadeInUp, FadeOutUp } from "react-native-reanimated";
 
 export default function App() {
   const [pressed, setPressed] = useState(false);
@@ -21,12 +20,7 @@ export default function App() {
       <TouchableOpacity onPress={handlePress} style={[styles.button,{backgroundColor: `${!color?"white":"grey"}`,}]}><Text style={{color: `${color?"white":"black"}`,fontWeight:800}}>Sample Button</Text></TouchableOpacity>
       {pressed?
        <View style={styles.textBox}>
-        {/* <Animated.View
-            entering={FadeInUp}
-            exiting={FadeOutUp}
-        > */}
-            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Text>
-        {/* </Animated.View> */}
+        <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Text>
        </View>
         : null
       }
