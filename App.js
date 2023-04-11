8d1e5b52bc7d8112903be1f59d7fea9a3d9371b4
ashish-tiwary13 <ashishtiwary1326@gmail.com>
2023-04-11 23:11:39 +0530

    initial

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+// import {Animated,FadeInUp, FadeOutUp } from "react-native-reanimated";
 
 export default function App() {
+  const [pressed, setPressed] = useState(false);
+  const [color, setColor] = useState(false);
+
+  handlePress = () => {
+    if(!pressed) {
+      setPressed(true);
+      setColor(true);
+    } else {
+      setPressed(false);
+      setColor(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={handlePress} style={[styles.button,{backgroundColor: `${!color?"white":"grey"}`,}]}><Text style={{color: `${color?"white":"black"}`,fontWeight:800}}>Sample Button</Text></TouchableOpacity>
+      {pressed?
+       <View style={styles.textBox}>
+        {/* <Animated.View
+            entering={FadeInUp}
+            exiting={FadeOutUp}
+        > */}
+            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Text>
+        {/* </Animated.View> */}
+       </View>
+        : null
+      }
     </View>
   );
 }
@@ -13,8 +37,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d5d7db',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    padding: 20,
+    borderRadius: 5,
+    width: "50%",
+    alignItems: 'center',
+  },
+  textBox: {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: 'white',
+    padding: 20,
+    width: "100%",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20, 
+   },
 });
